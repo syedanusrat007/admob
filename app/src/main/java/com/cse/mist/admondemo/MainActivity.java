fc11d5112e4a8073059e2172219819c4115d24da
syedanusrat007 <syedanusrat007@gmail.com>
2021-05-10 19:19:26 +0600

    admob app

---
+++ app/src/main/java/com/cse/mist/admondemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.cse.mist.admondemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-6820904809912762~3727197069");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+    }
+
+
+}
